525088590df1d72406cb38d8dd8fddd2ee5ca35b
unknown <arnav22112003@gmail.com>
2022-12-23 00:49:38 +0530

    added preview

--- props/src/components/Textarea.js
+++ props/src/components/Textarea.js
@@ -17,14 +17,23 @@ export default function Textarea(props) {
     }
     const [text,setText] = useState('Enter the text here');
     return (
-    <div>
+        <>
+    <div className='container'>
     <h1>{props.heading}</h1>
     <div className="mb-3">
     <textarea className="form-control" onChange={handleonchange} value={text} id="mybox" rows="8"></textarea>
     </div>
-    <button className="btn btn-primary" onClick={handleupclick}>Convert to uppercase</button>
-    <button className="btn btn-primary" onClick={handledownclick}>Convert to lowercase</button>
+    <button className="btn btn-primary mx-2" onClick={handleupclick}>Convert to uppercase</button>
+    <button className="btn btn-primary mx-2" onClick={handledownclick}>Convert to lowercase</button>
     </div>
+    <div className="container my-2">
+        <h1>Your text summary</h1>
+        <p>{text.split(" ").length} words {text.length} characters</p>
+        <p>{0.008 * text.split(" ").length} time to read </p>
+        <h2>preview</h2>
+        <p>{text}</p>
+    </div>
+    </>
   )
 
 }
