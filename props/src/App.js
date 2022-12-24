47ebce080fcf6d0f7e10aa31dcd1738b1266b44b
unknown <arnav22112003@gmail.com>
2022-12-24 11:48:02 +0530

    added dark mode for the website with toggle button

--- props/src/App.js
+++ props/src/App.js
@@ -1,14 +1,27 @@
 import './App.css';
 import Navbar from './components/Navbar';
 import Textarea from './components/Textarea';
+// import About from './components/About';
+import { useState } from 'react';
 
 function App() {
+  const[mode,setmode]=useState("light");
+  const togglemode=()=>{
+    if(mode==="dark"){
+    setmode("light");
+    document.body.style.backgroundColor="white";
+  }
+  else{
+    setmode("dark");
+    document.body.style.backgroundColor="black";
+  }
+  }
   return (
     <>
-    <Navbar title="Navbar"/>
+    <Navbar title="Navbar" mode={mode} togglemode={togglemode}/>
     <div className="container my-3">
-    <Textarea heading="Enter your text here" />
-
+    <Textarea heading="Enter your text here" mode={mode}/>
+    {/* <About/> */}
     </div>
     </>
   );
--- props/src/components/Textarea.js
+++ props/src/components/Textarea.js
@@ -18,20 +18,20 @@ export default function Textarea(props) {
     const [text,setText] = useState('Enter the text here');
     return (
         <>
-    <div className='container'>
+    <div className='container'style={{color:props.mode==="dark"?"white":"black"}}>
     <h1>{props.heading}</h1>
     <div className="mb-3">
-    <textarea className="form-control" onChange={handleonchange} value={text} id="mybox" rows="8"></textarea>
+    <textarea className="form-control" onChange={handleonchange} style={{backgroundColor:props.mode==="dark"?"black":"white",color:props.mode==="dark"?"white":"black"}}value={text} id="mybox" rows="8"></textarea>
     </div>
     <button className="btn btn-primary mx-2" onClick={handleupclick}>Convert to uppercase</button>
     <button className="btn btn-primary mx-2" onClick={handledownclick}>Convert to lowercase</button>
     </div>
-    <div className="container my-2">
+    <div className="container my-2" style={{color:props.mode==="dark"?"white":"black"}}>
         <h1>Your text summary</h1>
         <p>{text.split(" ").length} words {text.length} characters</p>
         <p>{0.008 * text.split(" ").length} time to read </p>
         <h2>preview</h2>
-        <p>{text}</p>
+        <p>{text.length>0?text:"enter some text to preview it"}</p>
     </div>
     </>
   )
