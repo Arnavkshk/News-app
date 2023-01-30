70db8ab05175ce01b88540764571d7bcfec42ab7
ARNAV KAUSHIK <arnav22112003@gmail.com>
2023-01-31 00:24:20 +0530

    added the title cards in news app

--- news/src/components/News.js
+++ news/src/components/News.js
@@ -4,9 +4,19 @@ import NewsItems from './NewsItems'
 export class News extends Component {
   render() {
     return (
-      <div>
-        This is a news component
+      <div className='container my-3'>
+        <h2>NewsApp - Top News Headlines</h2>
+        <div className="row">
+        <div className="col-md-4">
         <NewsItems/>
+        </div>
+        <div className="col-md-4">
+        <NewsItems/>
+        </div>
+        <div className="col-md-4">
+        <NewsItems/>
+        </div>
+        </div>
       </div>
     )
   }
--- news/src/components/NewsItems.js
+++ news/src/components/NewsItems.js
@@ -2,9 +2,17 @@ import React, { Component } from 'react'
 
 export class NewsItems extends Component {
   render() {
+    let {title, descripion}=this.props;
     return (
       <div>
-        This is news Item
+        <div class="card" style={{width: "18rem;"}}>
+          <img src="..." class="card-img-top" alt="..."/>
+          <div class="card-body">
+            <h5 class="card-title">{title}</h5>
+            <p class="card-text">{descripion}</p>
+            <a href="/" class="btn btn-primary">Go somewhere</a>
+          </div>
+        </div>
       </div>
     )
   }
