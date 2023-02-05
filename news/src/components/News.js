c0b2e57e2842b1dde3bbc2dcefdae90329f738af
ARNAV KAUSHIK <arnav22112003@gmail.com>
2023-02-05 09:06:02 +0530

    added buttons for next and previous

--- news/src/components/News.js
+++ news/src/components/News.js
@@ -29,6 +29,10 @@ export class News extends Component {
           </div>
         })}
         </div>
+        <div className="container d-flex justify-content-between">
+        <button type="button" class="btn btn-dark">Previous</button>
+        <button type="button" class="btn btn-dark">Next</button>
+        </div>
       </div>
     );
   }
