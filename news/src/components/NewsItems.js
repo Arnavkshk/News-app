7aebe07c43511d3163ea6f75857fe15eb6278c7a
ARNAV KAUSHIK <arnav22112003@gmail.com>
2023-02-09 18:20:47 +0530

    fixed some issues

--- news/src/components/NewsItems.js
+++ news/src/components/NewsItems.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react'
 
 export class NewsItems extends Component {
   render() {
-    let {title, description,imageurl,newsurl}=this.props;
+    let {title, description, imageurl ,newsurl}=this.props;
     return (
       <div className='my-3'>
         <div className="card" style={{width:"18rem"}}>
@@ -10,7 +10,7 @@ export class NewsItems extends Component {
           <div className="card-body">
             <h5 className="card-title">{title}...</h5>
             <p className="card-text">{description}...</p>
-            <a href={newsurl} target="_blank" className="btn btn-sm btn-dark" rel="noreferrer">Read More</a>
+            <a href={newsurl} target= "_blank" className="btn btn-sm btn-dark" rel="noreferrer">Read More</a>
           </div>
         </div>
       </div>
