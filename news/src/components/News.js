f766733b57916836848a3d55b3996369319d8592
ARNAV KAUSHIK <arnav22112003@gmail.com>
2023-02-24 18:08:24 +0530

    added loading gif when page is loading

--- news/src/components/News.js
+++ news/src/components/News.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import NewsItems from "./NewsItems";
+import Spinn from "./Spinn";
 
 export class News extends Component {
   
@@ -13,41 +14,44 @@ export class News extends Component {
   }
 
   async componentDidMount(){
+    this.setState({loading:true});
     let url=`https://newsapi.org/v2/everything?q=apple&from=2023-02-01&to=2023-02-01&sortBy=popularity&apiKey=d9484ba0267746dfba9de285f6c426bb&page=1&pageSize=${this.props.pageSize}`;
     let data = await fetch(url);
     let parsedData = await data.json();
     this.setState({articles: parsedData.articles})
+    this.setState({loading:false});
   }
 
   handlenexttext =async ()=>{
     console.log("next")
-    if(this.state.page+1>Math.ceil(this.state.totalResults/this.props.pageSize)){
-
-    }
-    else{
+    if(!(this.state.page+1>Math.ceil(this.state.totalResults/this.props.pageSize))){
+      this.setState({loading:true});
       let url=`https://newsapi.org/v2/everything?q=apple&from=2023-02-01&to=2023-02-01&sortBy=popularity&apiKey=d9484ba0267746dfba9de285f6c426bb&page=${this.state.page+1}&pageSize=${this.props.pageSize}`;
       let data = await fetch(url);
       let parsedData = await data.json();
+      this.setState({loading:false});
       this.setState({
         page: this.state.page+1,
-        articles: parsedData.articles
-      })
-    }
+        articles: parsedData.articles})
+      }
   }
   handleprevtext = async ()=>{
     console.log("prev");
+    this.setState({loading:true});
     let url=`https://newsapi.org/v2/everything?q=apple&from=2023-02-01&to=2023-02-01&sortBy=popularity&apiKey=d9484ba0267746dfba9de285f6c426bb&page=${this.state.page-1}&pageSize=${this.props.pageSize}`;
     let data = await fetch(url);
     let parsedData = await data.json();
+    this.setState({loading:false});
     this.setState({articles: parsedData.articles})
   }
-
+  
   render(){
     return(
       <div className="container my-3">
         <h2>NewsApp - Top News Headlines</h2>
+        {this.state.loading && <Spinn/>}
         <div className="row">
-        {this.state.articles.map((element)=>{ 
+        {!this.state.loading && this.state.articles.map((element)=>{ 
           return <div className="col-md-4" key={element.url}>
             <NewsItems title={element.title.slice(0,45)} description={element.description.slice(0,80)} imageurl={element.urlToImage} newsurl={element.url}/>
           </div>
