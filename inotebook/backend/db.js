d73175e1582983fbcdb96e6963427375028d211c
ARNAV KAUSHIK <arnav22112003@gmail.com>
2023-03-01 18:30:22 +0530

    fixed some issues with mongoose server in inotebook app

--- inotebook/backend/db.js
+++ inotebook/backend/db.js
@@ -1,8 +1,5 @@
 const mongoose = require('mongoose');
 const mongoURI="mongodb://localhost:27017"
 const connectToMongo=()=>{
-    mongoose.connect(mongoURI,()=>{
-        console.log("connected to mongo successfully");
-    });
-};
+    mongoose.connect(mongoURI,console.log("connected to mongo successfully"))};
 module.exports=connectToMongo;
