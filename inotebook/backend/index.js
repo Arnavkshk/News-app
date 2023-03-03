536d57ae4203bdb0b6892e50447a282caf09ee6e
ARNAV KAUSHIK <arnav22112003@gmail.com>
2023-03-03 17:56:18 +0530

    added new models to inotebook

--- inotebook/backend/index.js
+++ inotebook/backend/index.js
@@ -5,15 +5,9 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
-app.get('/api/v1/signup',(req,res)=>{
-  res.send("hello signup!")
-})
-app.get('/api/v1/login',(req,res)=>{
-  res.send("hello login!")
-})
+app.use('/api/auth', require('./routes/auth'))
+// app.use('/api/notes', require('./routes/notes'))
+
 app.listen(port, () => {
   console.log(`Example app listening on port http://localhost:${port}`)
 })
