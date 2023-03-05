bac698b3a7fdf7d21c4c07e203c5b41f211414b5
ARNAV KAUSHIK <arnav22112003@gmail.com>
2023-03-05 09:56:21 +0530

    added expres validation

--- inotebook/backend/routes/auth.js
+++ inotebook/backend/routes/auth.js
@@ -1,11 +1,22 @@
 const express=require('express');
 const router = express.Router();
 const User = require('../models/User');
+const { body, validationResult } = require('express-validator');
 
-router.post('/',(req,res)=>{
-    console.log(req.body);
-    const user=User(req.body);
-    user.save()
-    res.send(req.body);
+router.post('/',[
+    body('name',"name is too small").isLength({min : 3}),
+    body('email',"enter a valid email").isEmail(),
+    body('password',"password must be of atleast 5 characters").isLength({min:5})
+
+],(req,res)=>{
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+      }
+      User.create({
+        name: req.body.name,
+        email: req.body.email,
+        password:req.body.password,
+      }).then(user => res.json(user));
 })
 module.exports=router;
