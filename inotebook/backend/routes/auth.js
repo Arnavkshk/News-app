860d8d5d849c22929911a4eaeb2964c16dcf13f0
ARNAV KAUSHIK <arnav22112003@gmail.com>
2023-03-06 22:29:41 +0530

    added try catch for the errors

--- inotebook/backend/routes/auth.js
+++ inotebook/backend/routes/auth.js
@@ -1,26 +1,40 @@
-const express=require('express');
+const express = require("express");
 const router = express.Router();
-const User = require('../models/User');
-const { body, validationResult } = require('express-validator');
+const User = require("../models/User");
+const { body, validationResult } = require("express-validator");
 
-router.post('/',[
-    body('name',"name is too small").isLength({min : 3}),
-    body('email',"enter a valid email").isEmail(),
-    body('password',"password must be of atleast 5 characters").isLength({min:5})
-
-],async(req,res)=>{
+router.post(
+  "/",
+  [
+    body("name", "name is too small").isLength({ min: 3 }),
+    body("email", "enter a valid email").isEmail(),
+    body("password", "password must be of atleast 5 characters").isLength({
+      min: 5,
+    }),
+  ],
+  async (req, res) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
-        return res.status(400).json({ errors: errors.array() });
-      }
-      let user=User.findOne({email:req.body.email});
-      if(user){
-        return res.status(400).json({error:"Sorry a user with this email already exists"})
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    try {
+      let user = await User.findOne({ email: req.body.email });
+      if (user) {
+        return res
+          .status(400)
+          .json({ error: "Sorry a user with this email already exists" });
       }
       user = await User.create({
         name: req.body.name,
         email: req.body.email,
-        password:req.body.password,
-      }).then(user => res.json(user));
-})
-module.exports=router;
+        password: req.body.password,
+      });
+      res.json(user);
+    } catch (error) {
+      console.error(error.message);
+      res.status(500).send("some error occured");
+    }
+  }
+);
+module.exports = router;
