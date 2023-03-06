74e59fed8137c87f44d2f180093456db86f42aa1
ARNAV KAUSHIK <arnav22112003@gmail.com>
2023-03-06 19:49:03 +0530

    added async function in auth.js

--- inotebook/backend/routes/auth.js
+++ inotebook/backend/routes/auth.js
@@ -8,12 +8,16 @@ router.post('/',[
     body('email',"enter a valid email").isEmail(),
     body('password',"password must be of atleast 5 characters").isLength({min:5})
 
-],(req,res)=>{
+],async(req,res)=>{
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
         return res.status(400).json({ errors: errors.array() });
       }
-      User.create({
+      let user=User.findOne({email:req.body.email});
+      if(user){
+        return res.status(400).json({error:"Sorry a user with this email already exists"})
+      }
+      user = await User.create({
         name: req.body.name,
         email: req.body.email,
         password:req.body.password,
