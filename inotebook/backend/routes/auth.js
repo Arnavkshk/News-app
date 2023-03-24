61971fc81da9e8e43e1c01dfaf36d11698ebaeda
ARNAV KAUSHIK <arnav22112003@gmail.com>
2023-03-25 00:34:33 +0530

    fixed issue with update router component

--- inotebook/backend/routes/auth.js
+++ inotebook/backend/routes/auth.js
@@ -104,5 +104,15 @@ const {email,password}=req.body;
 
         const newNote={};
         if (title){newNote.title=title};
+        if (description){newNote.description=description};
+        if (tag){newNote.tag=tag};
+
+        let note= await Note.findById(req.params.id);
+        if(!note){return res.status(404).send("Not Found")}
+        if(note.user.toString() !== req.user.id){
+          return res.status(401).send("Not Allowed")
+        }
+        note=await Note.findByIdAndUpdate(req.params.id, {$set:newNote},{new:true})
+        res.json({note})
       })
 module.exports = router;
