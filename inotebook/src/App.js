e4be3cdae87419cfe3b90e54accbed40f0e22195
ARNAV KAUSHIK <arnav22112003@gmail.com>
2023-03-27 17:26:52 +0530

    made the navbar for inotebook

--- inotebook/src/App.js
+++ inotebook/src/App.js
@@ -1,10 +1,32 @@
 import './App.css';
+import Navbar from './components/Navbar';
+import React from "react";
+import Home from './components/Home';
+import About from './components/About';
+
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route
+} from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <>
+    <Router>
+    <Navbar/>
+    <Routes>
+          <Route exact path="/" element={ <Home/>}></Route> 
+          <Route exact path="/about" element={<About/>}></Route>
+          {/* <Route exact path="/general" element={}></Route>
+          <Route exact path="/entertainment" element={}></Route>
+          <Route exact path="/sports" element={}></Route>
+          <Route exact path="/science" element={}></Route>
+          <Route exact path="/health" element={}></Route>
+          <Route exact path="/technology" element={}></Route> */}
+    </Routes>
+    </Router>
+    </>
   );
 }
 
