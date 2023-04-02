d138c9148cf3bb887bea7f1b99c3045a0aab411b
ARNAV KAUSHIK <arnav22112003@gmail.com>
2023-04-02 11:40:08 +0530

    added add note function

--- inotebook/src/context/notes/notesState.js
+++ inotebook/src/context/notes/notesState.js
@@ -60,7 +60,17 @@ const NoteState = (props)=>{
         }
       ]
 
-      const addNote=(note)=>{
+      const addNote=(title,description
+        ,tag)=>{
+        const note= {
+          "_id": "64247f031206f3a7cb742119",
+          "user": "640f0d37dd183720945e228b",
+          "title": "my new added note",
+          "description": "new note added",
+          "tag": "personal sadjhf",
+          "date": "2023-03-29T18:10:11.296Z",
+          "__v": 0
+        };
         setNotes(notes.push(note))
 
       }
