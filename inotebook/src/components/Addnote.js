826ee1c6e55712e5e8d48c17618e8bbc886036f4
ARNAV KAUSHIK <arnav22112003@gmail.com>
2023-04-14 10:52:01 +0530

    added updatenote functionality

--- inotebook/src/components/Addnote.js
+++ inotebook/src/components/Addnote.js
@@ -19,17 +19,17 @@ const Addnote = () => {
         <form className='my-3'>
           <div className="mb-3">
             <label htmlFor="title" className="form-label">Title</label>
-            <input type="text" className="form-control" id="title" name="title" aria-describedby="emailHelp" onChange={onchange} />
+            <input type="text" className="form-control" id="title" name="title" aria-describedby="emailHelp" onChange={onchange} minLength={5} required/>
           </div>
           <div className="mb-3">
             <label htmlFor="description" className="form-label">Description</label>
-            <input type="text" className="form-control" id="description" name="description" onChange={onchange} />
+            <input type="text" className="form-control" id="description" name="description" onChange={onchange} minLength={5} required/>
           </div>
           <div className="mb-3">
             <label htmlFor="tag" className="form-label">Tag</label>
-            <input type="text" className="form-control" id="tag" name="tag" onChange={onchange} />
+            <input type="text" className="form-control" id="tag" name="tag" onChange={onchange} minLength={5} required/>
           </div>
-          <button type="submit" className="btn btn-primary" onClick={handleClick}>Add Note</button>
+          <button disabled={note.title.length<5 || note.description.length<5} type="submit" className="btn btn-primary" onClick={handleClick}>Add Note</button>
         </form>
 
       </div>
